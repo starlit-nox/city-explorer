4707f9b066ee54959ca4a74573f6c2ad972d1500
starlit-nox <kdkelly.1998@gmail.com>
2023-05-02 14:28:40 -0500

    Added Location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [searchquery, setSearchQuery] = useState('');
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    const getLocation = async () => {
+      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_ENV}&q=${searchquery}&format=json`;
+      console.log(API);
+      const res = await axios.get(API);
+      console.log(res.data[0])
+      setLocation(res.data[0]);
+    };
+
+    if (searchquery) {
+      getLocation();
+    }
+  }, [searchquery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={(e) => setSearchQuery(e.target.value)} placeholder="search for a city" />
+      <button>Explore Location!</button>
+      {location.place_id &&
+        <h2>The city is: {location.display_name}</h2>
+      }
+    </>
   );
 }
 
