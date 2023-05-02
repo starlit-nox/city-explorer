375db1c87fbed61012fdcd838d2b6563b0ebc33b
starlit-nox <kdkelly.1998@gmail.com>
2023-05-02 14:37:48 -0500

    newfix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import './App.css';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  const [searchquery, setSearchQuery] = useState('');
-  const [location, setLocation] = useState({});
+class App extends Component {
+  state = {
+    searchquery: '',
+    location: {}
+  };
 
-  useEffect(() => {
-    const getLocation = async () => {
-      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_ENV}&q=${searchquery}&format=json`;
-      console.log(API);
-      const res = await axios.get(API);
-      console.log(res.data[0])
-      setLocation(res.data[0]);
-    };
+  getLocation = async () => {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_ENV}&q=${this.state.searchquery}&format=json`;
+    console.log(API);
+    const res = await axios.get(API);
+    console.log(res.data[0])
+    this.setState({ location: res.data[0] });
+  };
 
-    if (searchquery) {
-      getLocation();
-    }
-  }, [searchquery]);
-
-  return (
-    <>
-      <input onChange={(e) => setSearchQuery(e.target.value)} placeholder="search for a city" />
-      <button>Explore Location!</button>
-      {location.place_id &&
-        <h2>The city is: {location.display_name}</h2>
-      }
-    </>
-  );
+  render() {
+    return (
+      <>
+        <input onChange={(e) => this.setState({ searchquery: e.target.value })} placeholder="search for a city"/>
+        <button onClick={this.getLocation}>Explore!</button>
+        {this.state.location.place_id && 
+          <h2>The city is: {this.state.location.display_name}</h2>
+        }
+      </>
+    );
+  }
 }
 
 export default App;
