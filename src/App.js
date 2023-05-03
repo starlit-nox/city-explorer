60b9029a225636446c89980f57d212edcdfb9a66
starlit-nox <kdkelly.1998@gmail.com>
2023-05-03 15:07:20 -0500

    Map

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-console.log(process.env.REACT_APP_MAP_KEY);
+import { Button, Form, Col, Row } from 'react-bootstrap';
+
 class App extends Component {
   state = {
     searchquery: '',
-    location: {}
+    location: { lat: null, lon: null, display_name: null },
+    mapUrl: ''
   };
 
   getLocation = async () => {
-    
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchquery}&format=json`;
-    
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchquery}&format=json`;
     const res = await axios.get(API);
-    console.log(res.data[0])
-    this.setState({ location: res.data[0] });
+    const { lat, lon, display_name } = res.data[0];
+    this.setState({ location: { lat, lon, display_name } });
+
+    const mapAPI = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${lat},${lon}&zoom=13`;
+    this.setState({ mapUrl: mapAPI });
   };
 
   render() {
     return (
-      <>
-        <input onChange={(e) => this.setState({ searchquery: e.target.value })} placeholder="search for a city"/>
-        <button onClick={this.getLocation}>Explore!</button>
-        {this.state.location.place_id && 
-          <h2>The city is: {this.state.location.display_name} @ {this.state.location.lat},{this.state.location.lon} @ {this.state.location.icon}</h2>
-          // added location for lat and lon above
-        }
-      </>
+      <div className="mt-5 container">
+        <Form>
+          <Form.Group as={Row} controlId="formSearch">
+            <Form.Label column sm={2}>
+              Search for a city
+            </Form.Label>
+            <Col sm={8}>
+              <Form.Control
+                type="text"
+                placeholder="Enter a city"
+                value={this.state.searchquery}
+                onChange={(e) => this.setState({ searchquery: e.target.value })}
+              />
+            </Col>
+            <Col sm={2}>
+              <Button variant="primary" onClick={this.getLocation}>
+                Explore!
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
+
+        {this.state.location.lat !== null && this.state.location.lon !== null && (
+          <div className="mt-3">
+            <h3>The city is: {this.state.location.display_name}!</h3>
+            <Row>
+              <Col>
+                <h4>Latitude:</h4>
+                <p>{this.state.location.lat}</p>
+              </Col>
+              <Col>
+                <h4>Longitude:</h4>
+                <p>{this.state.location.lon}</p>
+              </Col>
+            </Row>
+          </div>
+        )}
+
+        {this.state.mapUrl !== '' && (
+          <div className="mt-3">
+            <img src={this.state.mapUrl} alt="Map of city" />
+          </div>
+        )}
+      </div>
     );
   }
 }
